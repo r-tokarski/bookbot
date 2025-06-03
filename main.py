f0ba7b0bd94dc84aca345b5bcf5b00e7aa801e41
Radosław Tokarski <rtokarski@pm.me>
2025-06-03 18:15:23 +0200

    two defs for file reading

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book):
+    with open(book) as b:
+       return b.read()
+
+def main():
+    path = "books/frankenstein.txt"
+    print(get_book_text(path))
+
+main()
