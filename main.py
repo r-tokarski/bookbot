053a329744e0f93e81a3043703b739b74274eea5
Radosław Tokarski <rtokarski@pm.me>
2025-06-05 23:08:02 +0200

    solved but flawed

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from stats import get_book_text
+from stats import *
 
 def main():
     path = "books/frankenstein.txt"
+
     words = get_book_text(path).split()
     words_int = len(words)
-    print(f"{words_int} words found in the document")
+
+    chars = get_book_chars(path)
+    sorted = sort_chars(path)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_int} total words")
+    print("--------- Character Count -------")
+    #print(chars)
+    print(sorted)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,37 @@
 def get_book_text(book):
     with open(book) as b:
        return b.read()
+
+def get_book_chars(book):
+    alphabet = {}
+    text = get_book_text(book)
+    for char in text:
+        lower_case = char.lower()
+        if lower_case in alphabet:
+            alphabet[lower_case] += 1
+        else:
+            alphabet[lower_case] = 1
+
+    return alphabet
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_chars(book):
+    alphabet = get_book_chars(book)
+    list = []
+    result = ""
+    for alpha_char in alphabet:
+        if alpha_char.isalpha() == True:
+            string_char = f"{alpha_char}"
+            value_char = alphabet[alpha_char]
+            alpha_char = {}
+            alpha_char["char"] = string_char
+            alpha_char["num"] = value_char
+            list.append(alpha_char)
+    list.sort(reverse=True, key=sort_on)
+    for dict in list:
+        result = result + f"{dict["char"]}: {dict["num"]}\n"
+        if dict == list[-1]:
+            result = result + f"============= END ==============="
+    return result
